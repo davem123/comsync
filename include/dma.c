7b94907d7db7f25c70890aeb0023ceca903175c6
davem123 <dave@phas.ubc.ca>
2016-03-04 11:52:53 -0800

    Replace hard-coded memory locations with macros

--- include/dma.c
+++ include/dma.c
@@ -28,14 +28,14 @@ void dma_init(void) {
 	// DMA transfer triggered by TCC0 CCA (TAU0_VECT) interrupt
 	DMA.CH0.TRIGSRC = DMA_CH_TRIGSRC_TCC0_CCA_gc;
 
-	// Source: CLOCK0.CCA (TCD0.CCA)
-	DMA.CH0.SRCADDR0 = ( ( (uint16_t) 0x0928) >> 0 ) & 0xFF;
-	DMA.CH0.SRCADDR1 = ( ( (uint16_t) 0x0928) >> 8 ) & 0xFF;
+	// Source: CLOCK0.CCA
+	DMA.CH0.SRCADDR0 = ( ( (uint16_t) CLOCK0_CCA_ADDR) >> 0 ) & 0xFF;
+	DMA.CH0.SRCADDR1 = ( ( (uint16_t) CLOCK0_CCA_ADDR) >> 8 ) & 0xFF;
 	DMA.CH0.SRCADDR2 = 0;
 
-	// Destination: CLOCK0.CNT (TCD0.CNT)
-	DMA.CH0.DESTADDR0 = ( ( (uint16_t) 0x0920) >> 0 ) & 0xFF;
-	DMA.CH0.DESTADDR1 = ( ( (uint16_t) 0x0920) >> 8 ) & 0xFF;
+	// Destination: CLOCK0.CNT
+	DMA.CH0.DESTADDR0 = ( ( (uint16_t) CLOCK0_CNT_ADDR) >> 0 ) & 0xFF;
+	DMA.CH0.DESTADDR1 = ( ( (uint16_t) CLOCK0_CNT_ADDR) >> 8 ) & 0xFF;
 	DMA.CH0.DESTADDR2 = 0;
 
 
@@ -58,14 +58,14 @@ void dma_init(void) {
 	// DMA transfer triggered by TCC0 CCB (TAU1_VECT) interrupt
 	DMA.CH1.TRIGSRC = DMA_CH_TRIGSRC_TCC0_CCB_gc;
 
-	// Source: CLOCK1.CCA (TCD1.CCA)
-	DMA.CH1.SRCADDR0 = ( ( (uint16_t) 0x0968) >> 0 ) & 0xFF;
-	DMA.CH1.SRCADDR1 = ( ( (uint16_t) 0x0968) >> 8 ) & 0xFF;
+	// Source: CLOCK1.CCA
+	DMA.CH1.SRCADDR0 = ( ( (uint16_t) CLOCK1_CCA_ADDR) >> 0 ) & 0xFF;
+	DMA.CH1.SRCADDR1 = ( ( (uint16_t) CLOCK1_CCA_ADDR) >> 8 ) & 0xFF;
 	DMA.CH1.SRCADDR2 = 0;
 
-	// Destination: CLOCK1.CNT (TCD1.CNT)
-	DMA.CH1.DESTADDR0 = ( ( (uint16_t) 0x0960) >> 0 ) & 0xFF;
-	DMA.CH1.DESTADDR1 = ( ( (uint16_t) 0x0960) >> 8 ) & 0xFF;
+	// Destination: CLOCK1.CNT
+	DMA.CH1.DESTADDR0 = ( ( (uint16_t) CLOCK1_CNT_ADDR) >> 0 ) & 0xFF;
+	DMA.CH1.DESTADDR1 = ( ( (uint16_t) CLOCK1_CNT_ADDR) >> 8 ) & 0xFF;
 	DMA.CH1.DESTADDR2 = 0;
 
 	//====================
@@ -87,14 +87,14 @@ void dma_init(void) {
 	// DMA transfer triggered by TCC0 CCC (TAU2_VECT) interrupt
 	DMA.CH2.TRIGSRC = DMA_CH_TRIGSRC_TCC0_CCC_gc;
 
-	// Source: CLOCK2.CCA (TCF0.CCA)
-	DMA.CH2.SRCADDR0 = ( ( (uint16_t) 0x0B28) >> 0 ) & 0xFF;
-	DMA.CH2.SRCADDR1 = ( ( (uint16_t) 0x0B28) >> 8 ) & 0xFF;
+	// Source: CLOCK2.CCA
+	DMA.CH2.SRCADDR0 = ( ( (uint16_t) CLOCK2_CCA_ADDR) >> 0 ) & 0xFF;
+	DMA.CH2.SRCADDR1 = ( ( (uint16_t) CLOCK2_CCA_ADDR) >> 8 ) & 0xFF;
 	DMA.CH2.SRCADDR2 = 0;
 
-	// Destination: CLOCK2.CNT (TCF0.CNT)
-	DMA.CH2.DESTADDR0 = ( ( (uint16_t) 0x0B20) >> 0 ) & 0xFF;
-	DMA.CH2.DESTADDR1 = ( ( (uint16_t) 0x0B20) >> 8 ) & 0xFF;
+	// Destination: CLOCK2.CNT
+	DMA.CH2.DESTADDR0 = ( ( (uint16_t) CLOCK1_CNT_ADDR) >> 0 ) & 0xFF;
+	DMA.CH2.DESTADDR1 = ( ( (uint16_t) CLOCK1_CNT_ADDR) >> 8 ) & 0xFF;
 	DMA.CH2.DESTADDR2 = 0;
 
 	//====================
@@ -116,14 +116,14 @@ void dma_init(void) {
 	// DMA transfer triggered by TCC0 CCD (TAU3_VECT) interrupt
 	DMA.CH3.TRIGSRC = DMA_CH_TRIGSRC_TCC0_CCD_gc;
 
-	// Source: CLOCK3.CCA (TCF1.CCA)
-	DMA.CH3.SRCADDR0 = ( ( (uint16_t) 0x0B68) >> 0 ) & 0xFF;
-	DMA.CH3.SRCADDR1 = ( ( (uint16_t) 0x0B68) >> 8 ) & 0xFF;
+	// Source: CLOCK3.CCA
+	DMA.CH3.SRCADDR0 = ( ( (uint16_t) CLOCK3_CCA_ADDR) >> 0 ) & 0xFF;
+	DMA.CH3.SRCADDR1 = ( ( (uint16_t) CLOCK3_CCA_ADDR) >> 8 ) & 0xFF;
 	DMA.CH3.SRCADDR2 = 0;
 
-	// Destination: CLOCK3.CNT (TCF1.CNT)
-	DMA.CH3.DESTADDR0 = ( ( (uint16_t) 0x0B60) >> 0 ) & 0xFF;
-	DMA.CH3.DESTADDR1 = ( ( (uint16_t) 0x0B60) >> 8 ) & 0xFF;
+	// Destination: CLOCK3.CNT
+	DMA.CH3.DESTADDR0 = ( ( (uint16_t) CLOCK3_CNT_ADDR) >> 0 ) & 0xFF;
+	DMA.CH3.DESTADDR1 = ( ( (uint16_t) CLOCK3_CNT_ADDR) >> 8 ) & 0xFF;
 	DMA.CH3.DESTADDR2 = 0;
 
 	// Enable DMA controller, double buffer enabled on ch0/1 and ch2/3, 
--- include/dma.h
+++ include/dma.h
@@ -1,3 +1,16 @@
 #include "avr_compiler.h"
 
 void dma_init(void);
+
+// ===========================================================
+// Timer Configuration Macros
+// ===========================================================
+#define CLOCK0_CCA_ADDR		0x0928	//TCD0
+#define CLOCK1_CCA_ADDR		0x0968	//TCD1
+#define CLOCK2_CCA_ADDR		0x0B28	//TCF0
+#define CLOCK3_CCA_ADDR		0x0B68	//TCF1
+
+#define CLOCK0_CNT_ADDR		0x0920	//TCD0
+#define CLOCK1_CNT_ADDR		0x0960	//TCD1
+#define CLOCK2_CNT_ADDR		0x0B20	//TCF0
+#define CLOCK3_CNT_ADDR		0x0B60	//TCF1
